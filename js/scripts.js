b532b341d888dc40902c5cc42970dbbcc5e6fe40
Kevin BRANTONE <kbrantone@gmail.com>
2021-11-29 13:58:24 +0100

    page article ok

--- js/scripts.js
+++ js/scripts.js
@@ -5,31 +5,43 @@
 */
 
 const postsDiplay = document.getElementById("postsDisplay");
+const postTitle = document.getElementById("postTitle");
+const postContent = document.getElementById("postContent");
 
 window.addEventListener('load', ()=>{
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(posts => {
         posts.forEach(post =>{
-            const newDiv = document.createElement("div");
-            const newH2 = document.createElement("h2");
-            const newH3 = document.createElement("h3");
-            const newA = document.createElement("a");
-            const newHr = document.createElement("hr");
-            postsDiplay.appendChild(newDiv);
-            newDiv.appendChild(newA);
-            newA.appendChild(newH2);
-            newA.appendChild(newH3);
-            postsDiplay.appendChild(newHr);
-            newDiv.classList.add("post-preview");
-            newH2.classList.add("post-title");
-            newH3.classList.add("post-subtitle");   
-            newA.setAttribute("href", "post.html");
-            newH2.textContent = `${post.title}`;
-            newH3.textContent = `${post.body}`;
+            if(location.pathname=="/index.html"){
+                const newDiv = document.createElement("div");
+                const newH2 = document.createElement("h2");
+                const newH3 = document.createElement("h3");
+                const newA = document.createElement("a");
+                const newHr = document.createElement("hr");
+                const description = post.body.split(' ').slice(0, 3).join(' ');
+                postsDiplay.appendChild(newDiv);
+                newDiv.appendChild(newA);
+                newA.appendChild(newH2);
+                newA.appendChild(newH3);
+                postsDiplay.appendChild(newHr);
+                newDiv.classList.add("post-preview");
+                newH2.classList.add("post-title");
+                newH3.classList.add("post-subtitle");   
+                newA.setAttribute("href", "post.html" + "?" + post.id);
+                newH2.textContent = `${post.title}`;
+                newH3.textContent = `${description}...`;
+            }
+            else {
+                const postId = document.URL.toString().split("?")[1];
+                postTitle.textContent = `${posts[postId - 1].title}`;
+                postContent.textContent = `${posts[postId - 1].body}`;
+            }
         })
     });
 })
+
+
 
 window.addEventListener('DOMContentLoaded', () => {
     let scrollPos = 0;
